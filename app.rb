064ba1cb64f9ccedfd3008f581b0d7ee491cf95f
seron <seroncj@gmail.com>
2022-10-18 17:23:07 +0300

    Done

---
+++ app.rb
@@ -0,0 +1,16 @@
+# #puts adds line break at end of string.
+# puts "Hello World!"
+
+
+
+# #using print
+# print "Pass this text,please."
+
+
+
+# #Inspecting Data with p and pp(pretty printing)
+# puts [1, 2, 3]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
